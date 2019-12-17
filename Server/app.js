10015e953ee0cfe67ba76edae4db13ede511ddb5
Duc Trinh <dtrinh914@gmail.com>
2019-12-17 12:46:54 -0800

    set up socket.io

---
+++ Server/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('chat message', (msg)=>{
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(3000, ()=> {
+    console.log('Server has started');
+});
